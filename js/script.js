a8adc5215aede2d1197be0a78662d5b50dfb96ca
LarisaGimbu <gimbularisa@gmail.com>
2021-11-22 15:01:32 +0100

    emails array

--- js/script.js
+++ js/script.js
@@ -3,14 +3,23 @@ const app = new Vue({
   el:'#app',
 
   data:{
-
+    emails:[],
   },
 
   mounted(){
-    axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+
+    for(let i=0; i<10; i++){
+      axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
       .then((response) =>{
         const data = response.data;
-        console.log(data.response);
+        
+        this.emails.push(data.response);
+
+      })
+      .catch((error) =>{
+        console.log(error);
       })
   }
+    }
+    
 });
