30d9f0b3885356711b5fcf6b6757ccb35e110c90
LarisaGimbu <gimbularisa@gmail.com>
2021-11-22 15:04:37 +0100

    getEmails function

--- js/script.js
+++ js/script.js
@@ -6,20 +6,26 @@ const app = new Vue({
     emails:[],
   },
 
-  mounted(){
-
-    for(let i=0; i<10; i++){
-      axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-      .then((response) =>{
-        const data = response.data;
-        
-        this.emails.push(data.response);
-
-      })
-      .catch((error) =>{
-        console.log(error);
-      })
-  }
+  methods:{
+    getEmails(){
+      for(let i=0; i<10; i++){
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then((response) =>{
+          const data = response.data;
+          
+          this.emails.push(data.response);
+  
+        })
+        .catch((error) =>{
+          console.log(error);
+        })
+      }
     }
+  },
+
+  mounted(){
+    this.getEmails();
+   
+  }
     
 });
