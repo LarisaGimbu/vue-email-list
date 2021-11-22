48bb99bcdc82e5724b60b35a7eeefe227ff47c85
LarisaGimbu <gimbularisa@gmail.com>
2021-11-22 15:27:26 +0100

    loading

--- js/script.js
+++ js/script.js
@@ -4,17 +4,26 @@ const app = new Vue({
 
   data:{
     emails:[],
+    isLoading:true,
+    loadingText:'',
   },
 
   methods:{
     getEmails(){
       this.emails= [];
+      this.isLoading= true;
+      this.loadingText = 'Sto caricando...'
+
       for(let i=0; i<10; i++){
         axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
         .then((response) =>{
           const data = response.data;
           
           this.emails.push(data.response);
+
+          if(this.emails.length === 10){
+            this.isLoading = false;
+          }
   
         })
         .catch((error) =>{
