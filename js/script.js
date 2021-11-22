df4ed63a28608b7c25e75b79bed4ae3132d2ef60
LarisaGimbu <gimbularisa@gmail.com>
2021-11-22 14:54:00 +0100

    axios get

--- js/script.js
+++ js/script.js
@@ -3,6 +3,14 @@ const app = new Vue({
   el:'#app',
 
   data:{
-    
+
+  },
+
+  mounted(){
+    axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+      .then((response) =>{
+        const data = response.data;
+        console.log(data.response);
+      })
   }
 });
